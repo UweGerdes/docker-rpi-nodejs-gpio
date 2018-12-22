109d1a7bd1da93150647e95ee1132b599ba20e1c
Uwe Gerdes <entwicklung@uwegerdes.de>
2018-12-22 22:35:06 +0100

    adding boilerplate server for gpio

---
+++ modules/gpio/server/index.js
@@ -0,0 +1,16 @@
+/**
+ * Routes for gpio
+ *
+ * @module gpio/index
+ */
+
+'use strict';
+
+const router = require('express').Router(); // eslint-disable-line new-cap
+
+const controller = require('./controller.js');
+
+// gpio overview
+router.get('/', controller.index);
+
+module.exports = router;
---
+++ modules/gpio/server/model.js
@@ -0,0 +1,20 @@
+/**
+ * ## Model for gpio
+ *
+ * @module gpio/model
+ */
+
+'use strict';
+
+let data = { modelData: 'gpio data' };
+
+module.exports = {
+  /**
+   * get data
+   *
+   * @returns {object} model data
+   */
+  getData: () => {
+    return data;
+  }
+};
