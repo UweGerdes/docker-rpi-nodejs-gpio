01b71f46857dce382f902e8f482d19fab3d06af2
Uwe Gerdes <entwicklung@uwegerdes.de>
2018-12-29 15:02:39 +0100

    set value on range inputs

--- modules/gpio/js/index.js
+++ modules/gpio/js/index.js
@@ -18,6 +18,10 @@ function documentLoaded() {
   emitterElements.forEach((element) => {
     addEmitter(element);
   });
+  const valueElements = document.querySelectorAll('[value]');
+  valueElements.forEach((element) => {
+    addValueEmitter(element);
+  });
   const statusElements = document.querySelectorAll('.gpio-item-status[data-type=LED], .gpio-item-status[data-type=RGBLED]');
   statusElements.forEach((element) => {
     addStatusEmitter(element);
@@ -53,21 +57,19 @@ socket.on('item.data', (data) => {
     }
     if (element.classList.contains('gpio-item-status')) {
       if (data.data.type === 'LED') {
-        console.log('LED backgroundColor', rgb2hex({ [data.data.color]: data.data.pwmValue }));
         element.style.backgroundColor = rgb2hex({ [data.data.color]: data.data.pwmValue });
       } else if (data.data.type === 'RGBLED') {
-        // console.log('RGBLED color', element.classList, element.dataset.color, data.data.pwmValue[element.dataset.color]);
         element.style.backgroundColor = rgb2hex(data.data.pwmValue);
+      } else if (data.data.type === 'Sensor') {
+        element.classList.toggle('touched');
       } else {
         console.log('other status item.data', data);
       }
     }
     if (element.type === 'range') {
       if (data.data.type === 'LED') {
-        // console.log('LED item.data', data);
         element.value = data.data.pwmValue;
       } else if (data.data.type === 'RGBLED') {
-        // console.log('RGBLED color', element.classList, element.dataset.color, data.data.pwmValue[element.dataset.color]);
         element.value = data.data.pwmValue[element.dataset.color];
       } else if (data.data.type === 'Servo') {
         element.value = data.data.pwmValue;
@@ -91,6 +93,28 @@ function addEmitter(element) {
   });
 }
 
+function addValueEmitter(element) {
+  element.addEventListener('change', (event) => {
+    const value = event.currentTarget.value;
+    if (value.indexOf('#') === 0) {
+      socket.emit('setValue',
+        {
+          group: element.dataset.group,
+          item: element.dataset.name,
+          pwmValue: hex2rgb(value)
+        });
+    } else {
+      socket.emit('setValue',
+        {
+          group: element.dataset.group,
+          item: element.dataset.name,
+          color: element.dataset.color,
+          pwmValue: parseInt(value, 10)
+        });
+    }
+  });
+}
+
 function addStatusEmitter(element) {
   element.addEventListener('click', (event) => {
     if (event.currentTarget.getAttribute('data-status') === 'smooth') {
@@ -103,6 +127,21 @@ function addStatusEmitter(element) {
       socket.emit('smooth', { group: element.dataset.group, item: element.dataset.name, timeout: 2000 });
     }
   });
+}
+
+function hex2rgb(hex) {
+  // Expand shorthand form (e.g. '03F') to full form (e.g. '0033FF')
+  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, (m, r, g, b) => {
+    return r + r + g + g + b + b;
+  });
+
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    red: parseInt(result[1], 16),
+    green: Math.round(parseInt(result[2], 16) / 5),
+    blue: Math.round(parseInt(result[3], 16) / 5)
+  } : null;
 }
 
 function rgb2hex(color) {
